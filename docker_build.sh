00c2ad8936dce08cd31e295b0e6c7ec31823c16b
philippmarcus <marcus@cip.ifi.lmu.de>
2021-07-19 20:15:50 +0600

    Parameterization of parameters expanded

--- docker_build.sh
+++ docker_build.sh
@@ -10,7 +10,7 @@ echo $MAXMIND_KEY > maxmind.key
 chmod 400 maxmind.key
 
 # Build the Docker container
-DOCKER_BUILDKIT=1 docker build --progress=plain --secret id=maxmind,src=maxmind.key -t geovpn ./scripts
+DOCKER_BUILDKIT=1 docker build --env VPN_CERT_LOCATION=$VPN_CERT_LOCATION --progress=plain --secret id=maxmind,src=maxmind.key -t geovpn ./scripts
 
 # Remove key file
 rm -f maxmind.key
